5bdf81918a2bebf6a02aa482b7e6883952ab5d21
Emmett Keyser <emkeyser@gmail.com>
2017-12-26 14:09:57 -0800

    working api gateway call with cognito support and authorization token via auth0

--- src/containers/Admin/index.jsx
+++ src/containers/Admin/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 // import { PropTypes } from 'prop-types';
+import AWS from 'aws-sdk';
+import apigClientFactory from 'aws-api-gateway-client';
 import Users from '../../components/Users';
 import './index.css';
 
@@ -10,11 +12,37 @@ class Admin extends Component {
   }
 
   componentWillMount() {
-    fetch('https://api.***REMOVED***rx.io/cut/users')
-      .then(response => response.json())
-      .then((jsonResponse) => {
-        this.setState(() => ({ users: jsonResponse }));
-      });
+    const userPool = 'us-west-2:2440ab57-1a73-4701-91a1-0bfbf60a58a2';
+    const token = localStorage.getItem('id_token');
+    AWS.config.region = 'us-west-2';
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: userPool,
+      Logins: {
+        '***REMOVED***rx.auth0.com': token,
+      },
+    });
+
+    const self = this;
+    AWS.config.credentials.get(() => {
+      const config = {
+        // invokeUrl: 'https://6nkuz9s0m5.execute-api.us-west-2.amazonaws.com',
+        invokeUrl: 'https://api.***REMOVED***rx.io',
+        accessKey: AWS.config.credentials.accessKeyId,
+        secretKey: AWS.config.credentials.secretAccessKey,
+        sessionToken: AWS.config.credentials.sessionToken,
+        region: 'us-west-2',
+      };
+
+      const apigClient = apigClientFactory.newClient(config);
+
+      apigClient.invokeApi({}, '/cut/users', 'GET', {}, {})
+        .then((response) => {
+          self.setState(() => ({ users: response.data }));
+        })
+        .catch((err) => {
+          console.warn(err);
+        });
+    });
   }
 
   render() {
