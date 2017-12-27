f94d840116d259e6cbf1a9ba3c7666e0539122a4
Emmett Keyser <emkeyser@gmail.com>
2017-12-27 11:30:33 -0800

    latest

--- src/actions.jsx
+++ src/actions.jsx
@@ -26,14 +26,15 @@ export const handleAuthentication = () => (dispatch) => {
   });
 };
 
-export function loginUser() {
-  const webAuth = new auth0.WebAuth({
+export const loginUser = () => (dispatch) => {
+  new auth0.WebAuth({
     domain: '***REMOVED***rx.auth0.com',
     clientID: '0IiT2J2k96uMLgoFuq991mxCYOPytITk',
     redirectUri: `${window.location.protocol}//${window.location.host}/callback`,
     audience: 'https://***REMOVED***rx.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid',
-  });
-  webAuth.authorize();
-}
+  })
+    .authorize();
+  dispatch({ type: LOGIN_REQUEST });
+};
