ed4a0e3fef986f23d0e8106ebbb76577db25edef
Emmett Keyser <emkeyser@gmail.com>
2018-02-28 17:35:08 -0800

    wip

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,56 +3,56 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-	watch: true,
-	watchOptions: {
-		aggregateTimeout: 300,
-		poll: 500,
-	},
-	entry: './src/index.jsx',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index_bundle.js',
-		publicPath: '/',
-	},
-	devServer: {
-		// hot: true,
-		host: '0.0.0.0',
-		disableHostCheck: true,	// so you can use your phone - see https://github.com/webpack/webpack-dev-server/issues/882
-		publicPath: '/',
-		historyApiFallback: true,
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.jsx$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.scss/,
-				loaders: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.css/,
-				loaders: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				loader: 'url-loader?limit=10000',
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	plugins: [
-		new webpack.NoEmitOnErrorsPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({ template: './public/index.html' }),
-	],
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 500,
+  },
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    // hot: true,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    publicPath: '/',
+    historyApiFallback: true,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader?limit=10000',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+  ],
 };
