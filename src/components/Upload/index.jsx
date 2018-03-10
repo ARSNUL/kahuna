1942de4a94c8c5cd5b899634cbab282d9c2fc667
Emmett Keyser <emkeyser@gmail.com>
2018-03-10 07:26:45 -0800

    wip

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+// import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import './index.css';
+import LeftNav from '../../components/LeftNav';
+import { addUsers } from '../../actions/users';
+import { setIsLoading } from '../../actions/loadingdata';
 
-export default class Upload extends Component {
+class Upload extends Component {
   render() {
     return (
       <div className="Upload">
-        <div className="lander">
+        <LeftNav />
+        <div className="content">
           <h1>Upload</h1>
         </div>
       </div>
     );
   }
 }
+
+Upload.propTypes = {
+  // addUsers: PropTypes.func.isRequired,
+  // setIsLoading: PropTypes.func.isRequired,
+  // location: PropTypes.shape({
+  //   pathname: PropTypes.string,
+  //   search: PropTypes.string,
+  //   hash: PropTypes.string,
+  //   state: PropTypes.string,
+  // }),
+};
+
+Upload.defaultProps = {
+  // location: {},
+};
+
+function mapStateToProps() {
+  return {};
+}
+
+export default withRouter(connect(mapStateToProps, { addUsers, setIsLoading })(Upload));
