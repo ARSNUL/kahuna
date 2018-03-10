43d506a549d4ff69cd2de1d5d0de0db84ad58925
Emmett Keyser <emkeyser@gmail.com>
2018-03-10 14:38:02 -0800

    uploading working to s3 via lambda signing request handling

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 // import { PropTypes } from 'prop-types';
+import 'react-fine-uploader/gallery/gallery.css';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import FineUploaderS3 from 'fine-uploader-wrappers/s3';
+import Gallery from 'react-fine-uploader';
 import './index.css';
 import LeftNav from '../../components/LeftNav';
 import { addUsers } from '../../actions/users';
 import { setIsLoading } from '../../actions/loadingdata';
+
+const uploader = new FineUploaderS3({
+  options: {
+    request: {
+      endpoint: 'https://588439395328-kahuna-lake-v1.s3-us-west-2.amazonaws.com',
+      accessKey: 'AKIAIZC55BRNE7NHOIFA',
+      params: {
+        loggedinuser: '',
+      },
+    },
+    signature: {
+      endpoint: 'https://kahuna.arsnul.com/upload',
+    },
+    objectProperties: {
+      serverSideEncryption: true,
+    },
+    chunking: {
+      enabled: true,
+    },
+    validation: {
+      itemLimit: 50,
+      sizeLimit: 2000000000,
+    },
+  },
+});
 
 class Upload extends Component {
   render() {
     return (
       <div className="Upload">
         <LeftNav />
-        <div className="content">
-          <h1>Upload</h1>
-        </div>
+        <Gallery uploader={uploader} />
       </div>
     );
   }
