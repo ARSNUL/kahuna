8d962b49a07b68555da9d3d6ed5ba86e69a273c7
Emmett Keyser <emkeyser@gmail.com>
2018-03-12 16:51:25 -0700

    wip

---
+++ src/components/Configuration/index.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Configuration extends Component {
+  render() {
+    return (
+      <nav className="Configuration">
+        <ul>
+          <li>
+            <a className="NavItem NavSelected" href="/Admin">
+              <span>Users</span>
+            </a>
+          </li>
+          <li>
+            <a className="NavItem" href="/Inventory">
+              <span>Inventory</span>
+            </a>
+          </li>
+          <li>
+            <a className="NavItem" href="/Admin">
+              <span>Pipeline Management</span>
+            </a>
+          </li>
+          <li>
+            <a className="NavItem" href="/Upload">
+              <span>Upload</span>
+            </a>
+          </li>
+          <li>
+            <a className="NavItem" href="/Admin">
+              <span>Logs</span>
+            </a>
+          </li>
+          <li>
+            <a className="NavItem" href="/Spark">
+              <span>Spark</span>
+            </a>
+          </li>
+          <li>
+            <a className="NavItem" href="/Admin">
+              <span>Analytics</span>
+            </a>
+          </li>
+          <li>
+            <a className="NavItem" href="/Admin">
+              <span>Get Support</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Configuration;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,12 +10,7 @@ class Header extends Component {
       <header className="gb_xc gb_yd gb_Cd gb_lc">
         <div className="gb_Bd gb_8d gb_Id gb_ae">
           <div className="gb_yc gb_Td">
-            <div className="gb_ec">
-              <svg viewBox="0 0 24 24">
-                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-              </svg>
-            </div>
-            <span>Kahuna</span>
+            <h1>Kahuna</h1>
             <a className="Login" href="/Login">
               <span>Login</span>
             </a>
